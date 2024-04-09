21c1b85596b95a80d37fb085647d69366884bf7c
Harshal Tupe <harshaltupe12@gmail.com>
2024-04-09 19:12:05 +0530

    Create Layout file

---
+++ src/routes/layout/Layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import Navbar from "./components/navbar/Navbar.jsx"
+import Navbar from '../../components/navbar/Navbar';
+import {Outlet} from "react-router-dom";
+import "./layout.scss"
+
+function Layout() {
+  return (
+    <div className="layout">
+      <div className="navbar">
+        <Navbar/>
+      </div>
+      <div className="content">
+        <Outlet/>
+      </div>
+    </div>
+  )
+}
+
+export default Layout
