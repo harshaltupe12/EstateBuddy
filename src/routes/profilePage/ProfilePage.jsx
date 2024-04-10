d661c73bed70a58224f3a46ea599aba975949c54
Harshal Tupe <harshaltupe12@gmail.com>
2024-04-10 21:19:27 +0530

    Created Profile page. Work to be done

---
+++ src/routes/profilePage/ProfilePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./profilePage.scss"
+
+function ProfilePage() {
+  return (
+    <div className="profilePage">ProfilePage</div>
+  )
+}
+
+export default ProfilePage
