8289cc296fa632d2cffd110b676ad132627581c2
Harshal Tupe <harshaltupe12@gmail.com>
2024-04-12 22:35:06 +0530

    Started Building Backend

---
+++ client/src/App.jsx
@@ -0,0 +1,42 @@
+import Navbar from "./components/navbar/Navbar"
+import HomePage from "./routes/homePage/homePage"
+import Layout from "./routes/layout/Layout";
+import ListPage from "./routes/listPage/ListPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import SinglePage from "./routes/singlePage/SinglePage";
+import ProfilePage from "./routes/profilePage/ProfilePage";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children:[
+        {
+          path:"/",
+          element:<HomePage/>
+        },
+        {
+          path:"/list",
+          element:<ListPage/>
+        },
+        {
+          path:"/:id",
+          element:<SinglePage/>
+        },
+        {
+          path:"/profile",
+          element:<ProfilePage/>
+        },
+      ]
+    },
+  ]);
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default App
