372a6c9e948bd314511fce7334ad7ba36255dc86
Harshal Tupe <harshaltupe12@gmail.com>
2024-04-18 00:24:59 +0530

    Clear the repeated lines of request and Create Routes folder for them

--- api/app.js
+++ api/app.js
@@ -1,33 +1,10 @@
 import express from "express";
+import postRouter from "./routes/post.route.js";
+
 
 const app = express();
-app.use("/api/test", (req, res)=>{
-    res.send("It works")
-})
 
-app.use("/api/auth/register", (req, res)=>{
-    res.send("It Resister")
-})
-
-app.use("/api/auth/login", (req, res)=>{
-    res.send("It Login")
-})
-
-app.use("/api/auth/logout", (req, res)=>{
-    res.send("It Logout")
-})
-
-app.use("/api/posts/", (req, res)=>{
-    res.send("It Posts ")
-})
-
-app.use("/api/posts/", (req, res)=>{
-    res.send("It Posts")
-})
-
-app.use("/api/posts/123", (req, res)=>{
-    res.send("It Posts")
-})
+app.use("/api/posts", postRouter)
 
 app.listen(8800, ()=>{
     console.log("Server is running on http://localhost:8800")
