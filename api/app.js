8c20245a28cd860288778ac14952498efc227f42
Harshal Tupe <harshaltupe12@gmail.com>
2024-04-21 00:32:46 +0530

    ...

--- api/app.js
+++ api/app.js
@@ -2,14 +2,12 @@ import express from "express";
 import postRouter from "./routes/post.route.js";
 import authRouter from "./routes/auth.route.js";
 
-
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.use("/api/posts", postRouter)
-app.use("/api/auth", authRouter)
+app.use("/api/posts", postRouter);
+app.use("/api/auth", authRouter);
 
-
-app.listen(8800, ()=>{
-    console.log("Server is running on http://localhost:8800")
-})
+app.listen(8800, () => {
+  console.log("Server is running on http://localhost:8800");
+});
