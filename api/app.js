067ec599e124e0c7c03e898c5724d401f1754d45
Harshal Tupe <harshaltupe12@gmail.com>
2024-04-20 23:29:41 +0530

    Testing...

--- api/app.js
+++ api/app.js
@@ -1,10 +1,14 @@
 import express from "express";
 import postRouter from "./routes/post.route.js";
+import authRouter from "./routes/auth.route.js";
 
 
 const app = express();
+app.use(express.json())
 
 app.use("/api/posts", postRouter)
+app.use("/api/auth", authRouter)
+
 
 app.listen(8800, ()=>{
     console.log("Server is running on http://localhost:8800")
