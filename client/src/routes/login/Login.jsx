3b9c949c0775385a76c06b51276d5650a724fed6
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-03 17:40:41 +0530

    Created Login page

--- client/src/routes/login/Login.jsx
+++ client/src/routes/login/Login.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
-import "./login.scss"
+import axios from "axios";
+import "./login.scss";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import apiRequest from "../../lib/apiRequest";
+
 function Login() {
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    setIsLoading(true);
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
+    const password = formData.get("password");
+    console.log(username, password);
+    try {
+      const res = await axios.post("http://localhost:8800/api/auth/login", {
+        username,
+        password,
+      });
+      localStorage.setItem("user", JSON.stringify(res.data));
+      navigate("/")
+    } catch (err) {
+      setError(err.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
-    <div className="login">Login</div>
-  )
+    <div className="login">
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <h1>Welcome back</h1>
+          <input
+            name="username"
+            required
+            maxLength={20}
+            minLength={3}
+            type="text"
+            placeholder="Username"
+          />
+          <input
+            name="password"
+            required
+            maxLength={30}
+            minLength={6}
+            type="password"
+            placeholder="Password"
+          />
+          <button disabled={isLoading}>Login</button>
+          {error && <span>{error}</span>}
+          <Link to="/register">{"Don't"} you have an account?</Link>
+        </form>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
