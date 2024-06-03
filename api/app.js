dea1ac2f52e0004deae98ca8afc8edc0bc1592d2
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-03 17:37:35 +0530

    Cookie-parser & Cors added for accessing API

--- api/app.js
+++ api/app.js
@@ -1,9 +1,13 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import postRouter from "./routes/post.route.js";
 import authRouter from "./routes/auth.route.js";
 
 const app = express();
+app.use(cors({ origin: true, credentials: true}));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/posts", postRouter);
 app.use("/api/auth", authRouter);
