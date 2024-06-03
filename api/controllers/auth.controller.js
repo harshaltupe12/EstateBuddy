0b84d876687a3529ed36ecdbbe515ce8a93a3e95
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-03 17:38:44 +0530

    Authentication Done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,12 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma.js";
+import { json } from "express";
 export const register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword);
 
     // Create new user to database
     const newUser = await prisma.user.create({
@@ -24,26 +24,54 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   const { username, password } = req.body;
+
   try {
-    // Check if the user exist
+    // CHECK IF THE USER EXISTS
+
     const user = await prisma.user.findUnique({
       where: { username },
     });
-    console.log(user.password)
-    console.log(password)
+    console.log(user)
 
-    if (!user) return res.status(401).json({ message: "Invalid Credential-User" });
+    if (!user) return res.status(400).json({ message: "Invalid Credentials!" });
 
-    // Check the password
+    // CHECK IF THE PASSWORD IS CORRECT
+
     const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) return res.status(401).json({ message: "Invalid Credential!-Password" });
 
-    // Generate Cookie token and send it to user
-    res.setHeader("Set-Cookies", "text=" + "myValue").json("success")
+    if (!isPasswordValid)
+      return res.status(400).json({ message: "Invalid Credentials!" });
+
+    // GENERATE COOKIE TOKEN AND SEND TO THE USER
+
+    // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
+    const age = 1000 * 60 * 60 * 24 * 7;
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        isAdmin: false,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
+
+    const { password: userPassword, ...userInfo } = user;
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        // secure:true,
+        maxAge: age,
+      })
+      .status(200)
+      .json(userInfo);
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "Failed to login" });
+    res.status(500).json({ message: "Failed to login!" });
   }
 };
 
-export const logout = (req, res) => {};
+export const logout = (req, res) => {
+  res.clearCookie("token").status(200).json({ message: "Logout successfully" });
+};
