f8833c0c9daec2e70845f3425b1cf227436fbbe5
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-04 12:45:09 +0530

    Create a test Route

--- api/app.js
+++ api/app.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRouter from "./routes/post.route.js";
 import authRouter from "./routes/auth.route.js";
+import testRoute from "./routes/test.route.js";
+
 
 const app = express();
 app.use(cors({ origin: true, credentials: true}));
@@ -11,6 +13,7 @@ app.use(cookieParser());
 
 app.use("/api/posts", postRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/test", testRoute)
 
 app.listen(8800, () => {
   console.log("Server is running on http://localhost:8800");
