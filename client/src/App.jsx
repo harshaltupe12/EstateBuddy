c9cfa9f835a8d3b771a0f2e1bf2886d0b448dc6c
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-04 12:46:47 +0530

    Protected Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,59 @@
-import Navbar from "./components/navbar/Navbar"
-import HomePage from "./routes/homePage/homePage"
-import Layout from "./routes/layout/Layout";
+import Navbar from "./components/navbar/Navbar";
+import HomePage from "./routes/homePage/homePage";
+import { Layout, RequireAuth } from "./routes/layout/Layout";
 import ListPage from "./routes/listPage/ListPage";
 import Register from "./routes/register/Register";
-import Login from "./routes/login/Login"
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import Login from "./routes/login/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SinglePage from "./routes/singlePage/SinglePage";
 import ProfilePage from "./routes/profilePage/ProfilePage";
+import ProfileUpdatePage from "./routes/profileUpdatePage/ProfileUpdatePage";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout/>,
-      children:[
+      element: <Layout />,
+      children: [
         {
-          path:"/",
-          element:<HomePage/>
+          path: "/",
+          element: <HomePage />,
         },
         {
-          path:"/list",
-          element:<ListPage/>
+          path: "/list",
+          element: <ListPage />,
         },
         {
-          path:"/:id",
-          element:<SinglePage/>
+          path: "/:id",
+          element: <SinglePage />,
+        },
+
+        {
+          path: "/login",
+          element: <Login />,
         },
         {
-          path:"/profile",
-          element:<ProfilePage/>
+          path: "/register",
+          element: <Register />,
+        },
+      ],
+    },
+    {
+      path: "/",
+      element: <RequireAuth />,
+      children: [
+        {
+          path: "/profile",
+          element: <ProfilePage />,
         },
         {
-          path:"/login",
-          element:<Login/>
+          path: "/profile/update",
+          element: <ProfileUpdatePage />,
         },
-        {
-          path:"/register",
-          element:<Register/>
-        }
-      ]
+      ],
     },
   ]);
-  return (
-    <RouterProvider router={router}/>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
