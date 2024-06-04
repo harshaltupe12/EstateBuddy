242146f9735e3b668863bc0b292a52a44e4b862e
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-04 12:49:54 +0530

    Create function for Protected layout

--- client/src/routes/layout/Layout.jsx
+++ client/src/routes/layout/Layout.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
-// import Navbar from "./components/navbar/Navbar.jsx"
-import Navbar from '../../components/navbar/Navbar';
-import {Outlet} from "react-router-dom";
-import "./layout.scss"
+import React, { useContext, useEffect } from "react";
+import Navbar from "../../components/navbar/Navbar";
+import { Navigate, Outlet } from "react-router-dom";
+import "./layout.scss";
+import { AuthContext } from "../../context/AuthContext";
 
 function Layout() {
   return (
     <div className="layout">
       <div className="navbar">
-        <Navbar/>
+        <Navbar />
       </div>
       <div className="content">
-        <Outlet/>
+        <Outlet />
       </div>
     </div>
-  )
+  );
 }
 
-export default Layout
+function RequireAuth() {
+  const { currentUser } = useContext(AuthContext);
+
+  return !currentUser ? (
+    <Navigate to="/login" />
+  ) : (
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+export { Layout, RequireAuth };
