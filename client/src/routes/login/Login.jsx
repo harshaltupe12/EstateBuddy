6ae67f60095f1d0f4f372b80ac34960216a65c5c
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-04 12:50:21 +0530

    ...

--- client/src/routes/login/Login.jsx
+++ client/src/routes/login/Login.jsx
@@ -1,12 +1,15 @@
 import axios from "axios";
 import "./login.scss";
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import apiRequest from "../../lib/apiRequest";
+import { AuthContext } from "../../context/AuthContext";
 
 function Login() {
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const { updateUser } = useContext(AuthContext);
+
 
   const navigate = useNavigate();
   const handleSubmit = async (e) => {
@@ -21,7 +24,7 @@ function Login() {
         username,
         password,
       });
-      localStorage.setItem("user", JSON.stringify(res.data));
+      updateUser(res.data)
       navigate("/")
     } catch (err) {
       setError(err.response.data.message);
