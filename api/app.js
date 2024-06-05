2a33fa457d8d350c2f805cc425ecd3a165729de4
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-05 22:18:24 +0530

    Changes Done

--- api/app.js
+++ api/app.js
@@ -4,16 +4,18 @@ import cookieParser from "cookie-parser";
 import postRouter from "./routes/post.route.js";
 import authRouter from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
-
+import userRoute from "./routes/user.route.js";
 
 const app = express();
-app.use(cors({ origin: true, credentials: true}));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/posts", postRouter);
 app.use("/api/auth", authRouter);
-app.use("/api/test", testRoute)
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRouter);
+app.use("/api/test", testRoute);
+
 
 app.listen(8800, () => {
   console.log("Server is running on http://localhost:8800");
