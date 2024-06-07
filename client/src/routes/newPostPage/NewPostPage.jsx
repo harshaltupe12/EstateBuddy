b1ef45b491683f0af2f2278855a627f92db32c8e
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-07 21:14:07 +0530

    Getting error AxiosError

--- client/src/routes/newPostPage/NewPostPage.jsx
+++ client/src/routes/newPostPage/NewPostPage.jsx
@@ -20,40 +20,40 @@ function NewPostPage() {
     const inputs = Object.fromEntries(formData);
 
     try {
-      const res = await apiRequest.post("/posts", {
-        postData: {
-          title: inputs.title,
-          price: parseInt(inputs.price),
-          address: inputs.address,
-          city: inputs.city,
-          bedroom: parseInt(inputs.bedroom),
-          bathroom: parseInt(inputs.bathroom),
-          type: inputs.type,
-          property: inputs.property,
-          latitude: inputs.latitude,
-          longitude: inputs.longitude,
-          images: images,
+      const res = await apiRequest.post("/posts",{
+          postData: {
+            title: inputs.title,
+            price: parseInt(inputs.price),
+            address: inputs.address,
+            city: inputs.city,
+            bedroom: parseInt(inputs.bedroom),
+            bathroom: parseInt(inputs.bathroom),
+            type: inputs.type,
+            property: inputs.property,
+            latitude: inputs.latitude,
+            longitude: inputs.longitude,
+            images: images,
+          },
+          postDetail: {
+            desc: value,
+            utilities: inputs.utilities,
+            pet: inputs.pet,
+            income: inputs.income,
+            size: parseInt(inputs.size),
+            school: parseInt(inputs.school),
+            bus: parseInt(inputs.bus),
+            restaurant: parseInt(inputs.restaurant),
+          },
         },
-        postDetail: {
-          desc: value,
-          utilities: inputs.utilities,
-          pet: inputs.pet,
-          income: inputs.income,
-          size: parseInt(inputs.size),
-          school: parseInt(inputs.school),
-          bus: parseInt(inputs.bus),
-          restaurant: parseInt(inputs.restaurant),
-        },
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${currentUser.id}`,
+        {
+          headers: {
+            "Authorization": `Bearer ${currentUser.id}`,
+          },
         }
-      }
-    );
+      );
       navigate("/" + res.data.id);
     } catch (err) {
-      alert(err.message)
+      alert(err.message);
       console.log(err);
       setError(err);
     }
