f09b5a94605a0790b94818c4b7e9aa3f48addc39
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-08 21:40:52 +0530

    Fixing Cookie error

--- api/app.js
+++ api/app.js
@@ -7,12 +7,12 @@ import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
 
 const app = express();
-app.use(cors({origin: true, credentials: true,}))
+app.use(cookieParser());
+// app.use(cors({origin: true, credentials: true,}))
 
-// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
-app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRoute);
