70fe6af38ce9e8e292f22028065e9e736469879d
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-09 15:39:40 +0530

    Make Dynamic

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -4,8 +4,9 @@ import "leaflet/dist/leaflet.css";
 import Pin from '../pin/Pin';
 
 function Map({items}){
+  // [52.4797, -1.90269]
   return (
-    <MapContainer center={[52.4797, -1.90269]} zoom={6} scrollWheelZoom={false} className='map'>
+    <MapContainer center={items.length === 1 ? [items[0].latitude, items[0].longitude] : [52.4797, -1.90269] } zoom={6} scrollWheelZoom={false} className='map'>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
