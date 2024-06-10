5e3baab51ee995a74badeb94624cda80a3b28424
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-10 17:41:37 +0530

    Removing Unusable code

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -27,13 +27,8 @@ export const getUser = async (req, res) => {
 
 export const updateUser = async (req, res) => {
   const id = req.params.id;
-  // const id = req.body.token;
-  const body = req.body;
   const tokenUserId = req.userId;
-  const { password, avatar, ...inputs } = req.body;
-  console.log(id)
-  console.log(tokenUserId)
-  
+  const { password, avatar, ...inputs } = req.body;  
 
   if (id!== tokenUserId) {
     return res.status(403).json({ message: "User not able to update profile"});
@@ -79,5 +74,62 @@ export const deleteUser = async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to delete users!" });
+  }
+};
+
+export const savePost = async (req, res) => {
+  const postId = req.body.postId;
+  const tokenUserId = req.userId;
+
+  try {
+    const savedPost = await prisma.savedPost.findUnique({
+      where: {
+        userId_postId: {
+          userId: tokenUserId,
+          postId,
+        },
+      },
+    });
+
+    if (savedPost) {
+      await prisma.savedPost.delete({
+        where: {
+          id: savedPost.id,
+        },
+      });
+      res.status(200).json({ message: "Post removed from saved list" });
+    } else {
+      await prisma.savedPost.create({
+        data: {
+          userId: tokenUserId,
+          postId,
+        },
+      });
+      res.status(200).json({ message: "Post saved" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to delete users!" });
+  }
+};
+
+export const profilePosts = async (req, res) => {
+  const tokenUserId = req.params.userId;
+  try {
+    const userPosts = await prisma.post.findMany({
+      where: { userId: tokenUserId },
+    });
+
+    const saved = await prisma.savedPost.findMany({
+      where: { userId: tokenUserId },
+      include: {
+        post: true,
+      },
+    });
+    const savedPosts = saved.map((item) => item.post);
+    res.status(200).json({ userPosts, savedPosts });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get Profile Post!" });
   }
 };
