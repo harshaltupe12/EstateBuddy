2279097839c8ee4140f2fe681c9615bdac22c002
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-10 17:51:53 +0530

    Create function to handel data

--- client/src/routes/singlePage/SinglePage.jsx
+++ client/src/routes/singlePage/SinglePage.jsx
@@ -1,12 +1,38 @@
 import "./singlePage.scss";
 import Slider from "../../components/slider/Slider";
 import Map from "../../components/map/Map";
-import { singlePostData, userData } from "../../lib/dummyData";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import DOMPurify from 'dompurify';
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import apiRequest from "../../lib/apiRequest";
 
 function SinglePage() {
   const post = useLoaderData();
+  const [saved, setSaved] = useState(post.isSaved);
+  const navigate = useNavigate();
+  const { currentUser } = useContext(AuthContext);
+  // console.log(post)
+  const token = localStorage.getItem("token")
+  console.log(token)
+
+  const handleSave = async() =>{
+    if (!currentUser) {
+      navigate("/login");
+    }
+
+    setSaved((prev) => !prev);
+    try {
+      await apiRequest.post("/users/save", { postId: post.id }, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+    } catch (err) {
+      console.log(err)
+      setSaved((prev) => !prev);
+    }
+  }
   return (
     <div className="singlePage">
       <div className="details">
@@ -23,7 +49,7 @@ function SinglePage() {
                 <div className="price">$ {post.price}</div>
               </div>
               <div className="user">
-                <img src={post.user.avatar} alt="" />
+                <img src={post.user.avatar || "/noavatar.jpg"}  alt="" />
                 <span>{post.user.username}</span>
               </div>
             </div>
@@ -111,9 +137,11 @@ function SinglePage() {
               <img src="/chat.png" alt="" />
               Send a Message
             </button>
-            <button>
+            <button onClick={handleSave} style={{
+                backgroundColor: saved ? "#fece51" : "white",
+              }}>
               <img src="/save.png" alt="" />
-              Save the Place
+              { saved ? "Place is saved" : "Save the Place" }
             </button>
           </div>
         </div>
