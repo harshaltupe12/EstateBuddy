e81737c78fd5c2b5e50d45a1b1f70ae278227b34
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-10 17:49:51 +0530

    Create loader

--- client/src/lib/loaders.js
+++ client/src/lib/loaders.js
@@ -1,14 +1,38 @@
 import apiRequest from "./apiRequest"
 import { defer } from "react-router-dom";
 
-export const singlePageLoader = async({request, params}) =>{
-    const res = await apiRequest("/posts/"+params.id)
+// export const singlePageLoader = async({request, params}) =>{
+//     const token = localStorage.getItem("token");
+//     const res = await apiRequest("/posts/"+params.id)
+//     return res.data;
+// }
+
+export const singlePageLoader = async ({ request, params }) => {
+    const token = localStorage.getItem("token");
+    
+    if (!token) {
+      throw new Error("No token found");
+    }
+  
+    const res = await apiRequest(`/posts/${params.id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  
     return res.data;
-}
+  };
 
 export const listPageLoader = async({request, params}) =>{
     const query = request.url.split("?")[1];
     const postPromise = apiRequest("/posts?" + query)
+    return defer({
+        postResponse: postPromise
+    })
+}
+
+export const profilePageLoader = async() =>{
+    const postPromise = apiRequest("/users/profilePosts");
     return defer({
         postResponse: postPromise
     })
