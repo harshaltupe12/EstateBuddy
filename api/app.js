0b1cd7ac0e29bab1b639143a822e697e2c0350b5
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-11 14:49:40 +0530

    Creating Chat routes

--- api/app.js
+++ api/app.js
@@ -5,6 +5,9 @@ import postRouter from "./routes/post.route.js";
 import authRouter from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
+
 
 const app = express();
 app.use(cookieParser());
@@ -18,6 +21,9 @@ app.use("/api/auth", authRouter);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRouter);
 app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
+
 
 
 app.listen(8800, () => {
